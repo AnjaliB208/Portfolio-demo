2364f0187add018eed1154190da6ba38fa0e5400
AnjaliB208 <131346991+AnjaliB208@users.noreply.github.com>
2023-08-05 15:49:58 +0530

    Add files via upload
    Adding index.html, design.css and script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+
+    /*
+    //Create references to the input elements we wish to validate
+    var username = document.getElementById("name");
+    var email_address = document.getElementById("email");
+    //Check if username field is empty
+    if(username.value == ""){
+            alert("Please enter the name");
+            username.focus();
+            return false;
+    }
+    //Check if email field is empty
+    if(email_address.value == ""){
+            alert("Please enter the email");
+            email_address.focus();
+            return false;
+    }
+    //If all is well return true.
+    return true;
+    */
+    //const btnscroll = document.getElementById("scrollUp");
+    function addRec()
+    {
+        
+        var list = document.getElementById('recList');
+        var recommedation = document.getElementById("msg").value;
+        var entry = document.createElement('li');
+        entry.appendChild(document.createTextNode(recommedation));
+        list.appendChild(entry);
+        alert("Thanks for leaving a recommendation!")
+    }
+
+    function scrollUpwards()
+    {
+        window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "smooth"
+        });
+    }
